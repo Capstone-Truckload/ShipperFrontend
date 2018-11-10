7cf00e7f5f6bb07d326b463818e324da98c3d773
jbarber0 <jbarber0@users.noreply.github.com>
2018-11-10 14:40:51 -0600

    logout funct to hub

--- app/src/main/java/com/capstone/shipperfrontend/activities/HubActivity.java
+++ app/src/main/java/com/capstone/shipperfrontend/activities/HubActivity.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.capstone.shipperfrontend.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HubActivity extends AppCompatActivity {
 
     private Button CreateLoad;
     private Button ViewLoad;
     private Button Account;
+    private Button LogOut;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,7 @@ public class HubActivity extends AppCompatActivity {
         CreateLoad = (Button) findViewById(R.id.newLoadButton);
         ViewLoad = (Button) findViewById(R.id.currentButton);
         Account = (Button) findViewById(R.id.accountButton);
+        LogOut = (Button) findViewById(R.id.logoutButton);
 
         CreateLoad.setOnClickListener(new View.OnClickListener()
         {
@@ -44,6 +50,23 @@ public class HubActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(HubActivity.this, ManageAccountActivity.class));
+            }
+        });
+
+        LogOut.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.getInstance().signOut();
+
+                FirebaseUser user = firebaseAuth.getInstance().getCurrentUser();
+                if(user == null){
+                    Toast.makeText(HubActivity.this, "Logout Successful!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(HubActivity.this, LoginActivity.class));
+                }
+                else{
+                    Toast.makeText(HubActivity.this, "Logout Failed!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
