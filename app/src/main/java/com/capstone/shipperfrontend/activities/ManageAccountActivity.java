37b52ca7bf4759198049bd38b4d3a56d26e6dab2
jbarber0 <jbarber0@users.noreply.github.com>
2018-12-04 17:04:21 -0600

    finished manage account activity

--- app/src/main/java/com/capstone/shipperfrontend/activities/ManageAccountActivity.java
+++ app/src/main/java/com/capstone/shipperfrontend/activities/ManageAccountActivity.java
@@ -1,14 +1,19 @@
 package com.capstone.shipperfrontend.activities;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.capstone.shipperfrontend.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.android.gms.tasks.Task;
@@ -17,9 +22,10 @@ public class ManageAccountActivity extends AppCompatActivity {
 
     //String name;
     String email;
+    String current_;
 
     TextView userEmailtv;
-    EditText changeEmail, pass, confirmPass;
+    EditText changeEmail, pass, confirmPass, currentPassword;
     Button submit, delete;
     FirebaseUser user;
 
@@ -30,30 +36,86 @@ public class ManageAccountActivity extends AppCompatActivity {
         setupUIViews();
         retrieveUser();
         userEmailtv.setText(email);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email_ = changeEmail.getText().toString().trim();
+                String password_ = pass.getText().toString().trim();
+                String password_c = confirmPass.getText().toString().trim();
+                current_ = currentPassword.getText().toString().trim();
+
+                if(current_ != "" && email_ != "" && password_ == password_c && password_ != "") {
+                    reauthenticateUser(email, current_);
+                    updateEmail(email_);
+                    updatePassword(password_);
+                }
+                else if(current_ != "" && email_ != "") {
+                    reauthenticateUser(email, current_);
+                    updateEmail(email_);
+                }
+                else if(current_ != "" && password_ == password_c && password_ != "") {
+                    reauthenticateUser(email, current_);
+                    updatePassword(password_);
+                }
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                current_ = currentPassword.getText().toString().trim();
+                reauthenticateUser(email, current_);
+                deleteAccount();
+            }
+        });
     }
 
-    public void setEmail(){
-        user.updateEmail(changeEmail.toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+    public void updateEmail(String email_){
+        user.updateEmail(email_).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
                             //Log.d(TAG, "User email address updated.");
+                            Toast.makeText(ManageAccountActivity.this, "Updated email successful!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(ManageAccountActivity.this, LoginActivity.class));
                         }
+                        else
+                            Toast.makeText(ManageAccountActivity.this, "Current password incorrect!", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
 
     public void deleteAccount(){
-
+        user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            //Log.d(TAG, "User account deleted.");
+                            Toast.makeText(ManageAccountActivity.this, "Delete account successful!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(ManageAccountActivity.this, LoginActivity.class));
+                        }
+                        else
+                            Toast.makeText(ManageAccountActivity.this, "Current password incorrect!", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
-    public void updatePassword(){
-
+    public void updatePassword(String password_){
+        user.updatePassword(password_).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    //Log.d(TAG, "User email address updated.");
+                    Toast.makeText(ManageAccountActivity.this, "Update password successful!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(ManageAccountActivity.this, LoginActivity.class));
+                }
+                else
+                    Toast.makeText(ManageAccountActivity.this, "Current password incorrect!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    public void updateEmail(){
-
-    }
 
     void retrieveUser(){
         user = FirebaseAuth.getInstance().getCurrentUser();
@@ -67,12 +129,31 @@ public class ManageAccountActivity extends AppCompatActivity {
         }
     }
 
+    public void reauthenticateUser(String _email, String _password){
+        //user = FirebaseAuth.getInstance().getCurrentUser();
+
+        // Get auth credentials from the user for re-authentication. The example below shows
+        // email and password credentials but there are multiple possible providers,
+        // such as GoogleAuthProvider or FacebookAuthProvider.
+        AuthCredential credential = EmailAuthProvider.getCredential(_email, _password);
+
+        // Prompt the user to re-provide their sign-in credentials
+        user.reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //Log.d(TAG, "User re-authenticated.");
+                    }
+                });
+    }
+
     private void setupUIViews()
     {
         userEmailtv = (TextView)findViewById(R.id.email);
         changeEmail = (EditText)findViewById(R.id.changeEmail);
         pass = (EditText)findViewById(R.id.updatePassword);
         confirmPass = (EditText)findViewById(R.id.confirmPass);
+        currentPassword = (EditText)findViewById(R.id.currentPassword);
         submit = (Button)findViewById(R.id.update);
         delete = (Button)findViewById(R.id.delete);
     }
